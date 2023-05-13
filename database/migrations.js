689658efe9e608e498e86dc5a8deddfff2d9b7e5
crystal / ue <mfihy1@gmail.com>
2023-05-13 11:32:25 -0400

    Add db migration script

---
+++ database/migrations.js
@@ -0,0 +1,42 @@
+const { User, Avatar, Item} = require("./schemas");
+
+// {
+//     title: "fantage rookie",
+//     skin: 0,
+//     stickerPages: [ {
+//         stickerId: 12,
+//         x: 100,
+//         y: 60,
+//         flipX: false,
+//         givenBy: "bobbyBob123",
+//         date: "04/28/2023"
+//     },
+//     medals: [ {
+//         medalId: 3, // name and title retrieved by medalId (client side)
+//         level: 15,
+//         points: 236,
+//         nextLevel: 240,
+//     }]
+// }
+
+exports.userMigration = async function () {
+    try {
+      // Find and update documents
+      const documents = await User.find();
+      for (const doc of documents) {
+        // Add the new fields to each document
+        doc.idfone = {
+          title: 'fantage novice',
+          skin: 0,
+          stickerPages: [[], [], [], [], [], [], [], [], [], []],
+          medals: []
+        };
+  
+        // Save the updated document
+        await doc.save();
+      }
+      console.log('Documents updated successfully.');
+    } catch (error) {
+      console.error('Error updating documents:', error);
+    }
+  }
